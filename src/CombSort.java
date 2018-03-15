786efbe57d356c1b4db24b1afcd9932e3e32f01d
MarkSmirnov13 <marksmirnov13@gmail.com>
2018-03-15 23:33:09 +0300

    Generics + CompareTo + All types are available

--- src/CombSort.java
+++ src/CombSort.java
@@ -5,11 +5,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Scanner;
 
 // Java program for implementation of Comb Sort
 
-class CombSort
+class CombSort<T extends Comparable<T>>
 {
     // Function to sort arr[] using Comb Sort
 
@@ -18,7 +19,7 @@ class CombSort
      *
      * @param arr array to sort
      */
-    void comb(ArrayList<Integer> arr)
+    void comb(ArrayList<T> arr)
     {
         int step = arr.size() - 1;
         double fakt = 1.2473309;
@@ -34,7 +35,7 @@ class CombSort
             // Compare all elements with current gap
             // Swap arr[i] and arr[i + 1]
             for (int i = 0; i + step < arr.size(); i++)
-                if (arr.get(i) > arr.get(i + step))
+                if (arr.get(i).compareTo(arr.get(i + step)) > 0)
                     Collections.swap(arr, i, i + step);
             step /= fakt;
         }
@@ -43,7 +44,7 @@ class CombSort
         for (int i = 0; i < arr.size() - 1; i++)
         {
             for (int j = 0; j < arr.size() - i - 1; j++)
-                if (arr.get(i) > arr.get(i + 1))
+                if (arr.get(i).compareTo(arr.get(i + 1)) > 0)
                 {
                     // Swap arr[i] and arr[i+gap]
                     Collections.swap(arr, i, i + 1);
@@ -58,19 +59,21 @@ class CombSort
     // Driver method
     public static void main(String args[]) throws IOException {
         int a;
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<String> array = new ArrayList<String>();
 
         BufferedReader Fin = new BufferedReader(new FileReader("input.txt"));
         BufferedWriter Fout = new BufferedWriter(new FileWriter("output.txt"));
 
         CombSort ob = new CombSort();
         Scanner scanner = new Scanner(Fin);
-        a = scanner.nextInt();
+        scanner.useLocale(Locale.US); // start using English locale
 
-        array.ensureCapacity(a);
+        a = scanner.nextInt(); // scan number of elements
+
+        array.ensureCapacity(a); // resize the array
 
         for (int i = 0; i < a; i++)
-            array.add(i, scanner.nextInt());
+            array.add(i, scanner.next()); // scan the elements
 
         Fin.close();
 
